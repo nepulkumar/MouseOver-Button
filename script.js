88dd0cfcaac8ca6e08414b00ce625829590dbd99
nepulkumar <124512972+nepulkumar@users.noreply.github.com>
2023-10-02 23:10:23 +0530

    Add files via upload
    Final commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// mouseOver on me event listener on button
+
+const boxElement = document.getElementById("box");
+
+//adding mouseover event listener
+
+boxElement.addEventListener("mouseover", function() {
+    alert("this is restricted army area (mouse over is working properly)");
+});
